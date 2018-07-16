142f2dce13b05bb549dc48e1389480c7d2f11f1c
isutare412 <isutare412@xlgames.com>
2018-07-16 18:31:22 +0900

    bug fix changed variable name

--- Baekjoon/tmp/history0010.py
+++ Baekjoon/tmp/history0010.py
@@ -6,8 +6,8 @@ test_size = int(input())
 
 for _ in range(test_size):
     size, *scores = list(map(lambda s: int(s), input().strip().split()))
-    mean = mean(scores)
+    score_mean = mean(scores)
 
-    above_mean = sum(1 for s in scores if s > mean)
+    above_mean = sum(1 for s in scores if s > score_mean)
     ratio = above_mean / size
     print('{:.3f}%'.format(ratio * 100))
